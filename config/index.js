cc2cf37a0499e694be1ff8ff76f9c4720d2fd653
p-kat <88550842+p-kat@users.noreply.github.com>
2021-08-09 21:04:20 +0300

    dist

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/web'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-text-highlight/',
+    assetsPublicPath: '/vue-text-highlight-international/',
 
     /**
      * Source Maps
